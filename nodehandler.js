2a3226b8f0a516bfc3507ea3a0a371bac198b078
Sirawit <plurm2545@hotmail.com>
2021-07-31 06:25:47 +0700

    feat: upload handler

--- nodehandler.js
+++ nodehandler.js
@@ -1,21 +1,61 @@
 const { createClient } = require("@supabase/supabase-js");
+const { decode } = require("base64-arraybuffer");
+const { v4 } = require("uuid");
 
-// Create a single supabase client for interacting with your database
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
 
 module.exports.upload = async (event) => {
-  const { user, session, error } = await supabase.auth.signIn({
+  const body = JSON.parse(event.body);
+  const {
+    user,
+    session,
+    error: userError,
+  } = await supabase.auth.signIn({
     email: process.env.ADMIN_EMAIL,
     password: process.env.ADMIN_PASSWORD,
   });
+  if (userError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: userError }),
+    };
+  }
+  const fileName = v4();
+  const filedata = body.filedata;
+  const mimeType = filedata.slice(
+    filedata.indexOf(":") + 1,
+    filedata.indexOf(";")
+  );
+  const base64data = filedata.replace(/^data:image\/\w+;base64,/, "");
+  const { data, error: storageError } = await supabase.storage
+    .from("default-bucket")
+    .upload(`admin/${fileName}`, decode(base64data), {
+      contentType: mimeType,
+    });
+  if (storageError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: storageError }),
+    };
+  }
+  const { signedURL, error: signedURLError } = await supabase.storage
+    .from("default-bucket")
+    .createSignedUrl(`admin/${fileName}`, 3600);
+  if (signedURLError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: signedURLError }),
+    };
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: user,
+        name: fileName,
+        url: signedURL,
       },
       null,
       2
